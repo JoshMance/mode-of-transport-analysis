1368b0f2788839b59746734d1732b882ee3ba5da
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-12-11 12:45:15 +0800

    Now reading xlsx

--- data-sourcing-and-cleaning/melb_and_syd_populations.py
+++ data-sourcing-and-cleaning/melb_and_syd_populations.py
@@ -1,10 +1,6 @@
 import requests
 import pandas as pd
 
-url = 'https://www.abs.gov.au/articles/50-years-capital-city-population-change'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-len(df_list)
-df = df_list[-3]
-print(df)
-df.to_csv('my data.csv')
+# https://www.abs.gov.au/statistics/labour/employment-and-unemployment/labour-force-australia/latest-release#data-downloads
+
+pd.read_excel('data/ABS_Victoria_Labour_Force.xlsx', index_col=0) 
