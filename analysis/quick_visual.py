7b25b2316684faffc6f8df5397b5d73370e4275a
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-12-11 14:53:15 +0800

    Adding inflation adjustments

--- analysis/quick_visual.py
+++ analysis/quick_visual.py
@@ -2,11 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 employment_df = pd.read_csv('data/cleaned-data/wa_employment_data.csv')
-fuel_price_df = pd.read_csv('data/cleaned-data/pert_metro_fuel_prices.csv')
+fuel_price_df = pd.read_csv('data/cleaned-data/perth_metro_fuel_prices.csv')
 
-print(employment_df)
-print(fuel_price_df)
+df = employment_df.merge(fuel_price_df, on=['Year', 'Month'])
+df = df.drop(['Unnamed: 0_x', 'Unnamed: 0_y'], axis=1)
+
+employment_and_price = list(zip(df['ULP Price'], df['Number of Employed People']))
+employment_and_price.sort()
+
+y = [val[1] for val in employment_and_price]
+x = [val[0] for val in employment_and_price]
 
 plt.figure()
-plt.plot()
+plt.scatter(x, y)
 plt.show()
--- cleaning/perth_metro_fuel_prices.py
+++ cleaning/perth_metro_fuel_prices.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import calendar
+import ausdex
 
 PATH = "data/source-data/perth_metro_monthly_ulp_prices.csv"
-DATES_COLUMN_NAME = 'Month'
-PRICE_COLUMN_NAME = 'Average'
+DATES_COLUMN = 'Month'
+PRICE_COLUMN = 'Average'
 
 df = pd.read_csv(PATH, header=1)
 df = df[::-1]
 
-dates = df[DATES_COLUMN_NAME]
+dates = df[DATES_COLUMN]
+
+# Adjusting for inflation and converting to dollars
+prices = [ausdex.calc_inflation(price, dates[i], location="perth")
+         for i, price in enumerate(df[PRICE_COLUMN])]
+
+prices = [val/100 for val in prices]
 
 month_names_to_nums = {month: index for index, month in enumerate(calendar.month_name) if month}
 months = [month_names_to_nums[date.split(' ')[0]] for date in dates]
@@ -16,8 +23,11 @@ years = [date.split(' ')[1] for date in dates]
 
 new_df = pd.DataFrame(data = {'Year' : years,
                               'Month': months,
-                              'ULP Price': df[PRICE_COLUMN_NAME]
+                              'ULP Price': prices
                              }
                      )
+
+
+
 
 new_df.to_csv('data/cleaned-data/perth_metro_fuel_prices.csv')
