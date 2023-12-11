5015a5a894032ccaacf9965936b5403fee4c066a
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-12-11 13:19:03 +0800

    Cleaning ABS Vic employment data

--- sourcing-and-cleaning/vic_employment_data.py
+++ sourcing-and-cleaning/vic_employment_data.py
@@ -1,6 +1,29 @@
-import requests
 import pandas as pd
 
 # https://www.abs.gov.au/statistics/labour/employment-and-unemployment/labour-force-australia/latest-release#data-downloads
 
-pd.read_excel('data/ABS_Victoria_Labour_Force.xlsx', index_col=0) 
+SHEET_NAME = 'Data1'
+YEAR_COLUMN_NAME = 'Unnamed: 0'
+NUM_COLUMN_NAME = "Employed total ;  Persons ;"
+HEADER_DATA_OFFSET = 9
+
+df = pd.read_excel('data/ABS_Victoria_Labour_Force.xlsx', 
+                   sheet_name=[SHEET_NAME]
+                   ).pop(SHEET_NAME)
+
+
+dates = [str(date).split(" ")[0]
+         for date in df[YEAR_COLUMN_NAME][HEADER_DATA_OFFSET:]]
+years = [date.split("-")[0] for date in dates]
+months = [date.split("-")[1] for date in dates]
+
+number_employed_people = [num*1000 for num in df[NUM_COLUMN_NAME][HEADER_DATA_OFFSET:]]
+
+
+new_df = pd.DataFrame(data = {'Year' : years,
+                              'Month': months,
+                              'Number of Employed People': number_employed_people
+                             }
+                     )
+
+print(new_df)
